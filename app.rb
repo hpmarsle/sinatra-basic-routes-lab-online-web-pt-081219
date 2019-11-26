d681014de366c32f04369a28f7ebe4e626d4adb6
Mars Le <hp.mars.le@gmail.com>
2019-11-26 17:39:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,4 +4,12 @@ class App < Sinatra::Base
   get '/name' do 
     "My name is Maria!"
   end 
+  
+  get '/hometown' do 
+    "My hometown is Dallas."
+  end 
+  
+  get 'favorite-song' do 
+    'My favorite song is Soulmate'
+  end
 end
